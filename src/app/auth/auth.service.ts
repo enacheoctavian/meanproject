6340ebdea8b8ff31488275e10725ee19cacc9373
Enache Octavian <enacheioan11@yahoo.com>
2022-01-04 20:31:36 +0000

    extending the token duration to 1h

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,6 +7,7 @@ import { AuthData } from "./auth-data.model";
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
+    private tokenTimer: any;
     private isAuthenticated = false;
     private token: string;
     private authStatusListener = new Subject<boolean>();
@@ -37,22 +38,88 @@ export class AuthService {
     login(email: string, password: string) {
         const authData: AuthData = { email: email, password: password };
         this.http
-            .post<{ token: string }>("http://localhost:3000/api/user/login", authData)
+            .post<{ token: string, expiresIn: number }>("http://localhost:3000/api/user/login", authData)
             .subscribe(response => {
                 const token = response.token;
                 this.token = token;
                 if (token) {
+                    const expiresInDuration = response.expiresIn;
+                    this.setAuthTimer(expiresInDuration);
                     this.isAuthenticated = true;
                     this.authStatusListener.next(true);
+                    const now = new Date();
+                    const expirationDate = new Date(now.getTime() + expiresInDuration * 1000);
+                    this.saveAuthData(token, expirationDate)
                     this.router.navigate(['/']);
                 }
             });
     }
 
+    autoAuthUser() {
+
+        const authInformation = this.getAuthData();
+        if (!authInformation) {
+            return;
+        }
+        const now = new Date();
+        const expiresIn = authInformation.expirationDate.getTime() - now.getTime();
+        if (expiresIn > 0) {
+            this.token = authInformation.token;
+            this.isAuthenticated = true;
+            this.setAuthTimer(expiresIn / 1000);
+            this.authStatusListener.next(true);
+
+        }
+    }
     logout() {
         this.token = null;
         this.isAuthenticated = false;
         this.authStatusListener.next(false);
+        clearTimeout(this.tokenTimer);
         this.router.navigate(['/']);
+    }
+
+    // private getAuthData() {
+    //     let ok = 1;
+    //     const token = localStorage.getItem("token");
+    //     const expirationDate = localStorage.getItem("expiration");
+    //     if (!token || !expirationDate) {
+    //         ok = 0;
+    //     }
+    //     if (ok === 1) {
+    //         return {
+    //             token: token,
+    //             expirationDate: new Date(expirationDate)
+    //         }
+    //     }
+    //     return;
+
+    // }
+    private getAuthData() {
+        const token = localStorage.getItem("token");
+        const expirationDate = localStorage.getItem("expiration");
+        if (!token || !expirationDate) {
+            return null;
+        }
+        return {
+            token: token,
+            expirationDate: new Date(expirationDate),
+        };
+    }
+
+    private saveAuthData(token: string, expirationDate: Date) {
+        localStorage.setItem('token', token);
+        localStorage.setItem('expiration', expirationDate.toISOString());
+    }
+
+    private clearAuthData() {
+        localStorage.removeItem('token');
+        localStorage.removeItem('expiration');
+    }
+    private setAuthTimer(duration: number) {
+        console.log("Setting timer: " + duration)
+        this.tokenTimer = setTimeout(() => {
+            this.logout();
+        }, duration * 1000);
     }
 }
